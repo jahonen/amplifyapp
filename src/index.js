7912a647e12e617d21f90d0b31cbede7f7b2182a
jahonen <jukkisahonen@gmail.com>
2020-09-22 13:29:13 +0300

    included aws-exports config

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+
+Amplify.configure(    
+  "aws_project_region": "eu-west-1",
+  "aws_cognito_identity_pool_id": "eu-west-1:766f3384-fd7c-4472-95e0-3e4a9fe11c08",
+  "aws_cognito_region": "eu-west-1",
+  "aws_user_pools_id": "eu-west-1_mG301YzUr",
+  "aws_user_pools_web_client_id": "1h0lcj1d83k2m4ddn6pdfsbic4",
+  "oauth": {},
+  "aws_appsync_graphqlEndpoint": "https://hq7kqlp6q5czbncosk6l6gx6wu.appsync-api.eu-west-1.amazonaws.com/graphql",
+  "aws_appsync_region": "eu-west-1",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": "da2-i3bdxgjwwrgpfkurebw25xgtvi",
+  "aws_user_files_s3_bucket": "bucket103628-dev",
+  "aws_user_files_s3_bucket_region": "eu-west-1"
+);
 
 ReactDOM.render(
   <React.StrictMode>
